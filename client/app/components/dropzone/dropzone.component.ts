86694a49d500a0d94bc688bdf92dd407025b60f9
Robert Urban <robert.urban@outlook.de>
2019-01-09 02:11:41 +0100

    Add Error Handling

--- client/app/components/dropzone/dropzone.component.ts
+++ client/app/components/dropzone/dropzone.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 import { DropzoneDirective, DropzoneConfigInterface } from 'ngx-dropzone-wrapper';
 
 
@@ -18,9 +18,12 @@ export class DropzoneComponent implements OnInit {
   /* Upload UI states */
   private fileLoading = true;
   private successfulUpload = false;
-  private errorUpload = false;
   private openUploadModal = false;
   private uploadModalMinified = true;
+
+  @ViewChild(DropzoneDirective) directiveRef: DropzoneDirective;
+
+  constructor(){ }
 
   public config: DropzoneConfigInterface = {
     createImageThumbnails: false,
@@ -30,31 +33,31 @@ export class DropzoneComponent implements OnInit {
     previewsContainer: "#dropzone-file-previews",
     clickable: "#dropzone-upload-button",
     previewTemplate: `
-      <div class="dz-preview dz-file-preview">
-        <div class="dz-details">
-          <div class="dz-filename"><span data-dz-name></span></div>
-          <div class="dz-size" data-dz-size></div>
-        </div>
-        <div *ngIf="fileLoading" class="dz-progress">
-          <fa name="close" animation="spin"></fa>
-        </div>
-        <div *ngIf="successfulUpload" class="dz-success-mark">
-          <fa name="success"></fa>
-        </div>
-        <div *ngIf="errorUpload" class="dz-error-mark">
-          <fa name="exclamation-circle"></fa>
-        </div>
-        <div class="dz-error-message"><span data-dz-errormessage></span></div>
+    <div class="dz-preview dz-file-preview">
+      <div class="dz-details">
+        <div class="dz-filename"><span data-dz-name></span></div>
       </div>
-    `,
+      <div class="dz-icons">
+        <div id="dz-spinner" class="dz-progress">
+          <i class="fa fa-circle-o-notch fa-spin"></i>
+        </div>
+        <div id="dz-success" class="dz-success-mark">
+          <i class="fa fa-check"></i>
+        </div>
+      </div>
+      <div id="dz-error-group">
+        <div id="dz-error" class="dz-error-mark">
+          <i class="fa fa-exclamation-circle"></i>
+        </div>
+        <div id="dz-error-message" class="dz-error-message"><span data-dz-errormessage></span></div>
+      </div>
+    </div>
+  `,
     cancelReset: null
   };
 
-  @ViewChild(DropzoneDirective) directiveRef?: DropzoneDirective;
-
-  constructor(){}
-
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   public onDragEnter(args: any): void {
     event.stopPropagation();
@@ -78,20 +81,34 @@ export class DropzoneComponent implements OnInit {
       event.preventDefault();
       this.dragged = false;
     }
+
   }
 
   public onUploadSuccess(args: any): void {
     this.updateFileList = !this.updateFileList;
     this.successfulUpload = true;
+    // REFACTOR: Ugly workaound as Angular Bindings are not working with PreviewTemplate
+    document.querySelector('#dz-spinner').style.display="none";
+    document.querySelector('#dz-success').style.display="block";
   }
 
+  public onUploadError(file, message){
+    var errorNode = document.querySelector('.dz-error');
+    if(errorNode){
+      errorNode.querySelector('#dz-spinner').style.display="none";
+      errorNode.querySelector('#dz-error-group').style.display="block";
+    }
+  }
+
+
   public onFileAdded(args: any): void {
-    this.onDropped();
+    this.onDropped(true);
   }
 
   public closeModal(){
     if (this.successfulUpload){
-        this.resetUIstate();
+      this.resetUIstate();
+      this.directiveRef.reset();
     }
   }
 
@@ -102,9 +119,8 @@ export class DropzoneComponent implements OnInit {
   public resetUIstate(){
     this.fileLoading = true;
     this.successfulUpload = false;
-    this.errorUpload = false;
     this.openUploadModal = false;
-    this.uploadModalMinified = false;
+    this.uploadModalMinified = true;
   }
 
 
